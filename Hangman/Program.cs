7c15312748a3df726f5a075bdd274f1d17730126
craig2812 <craig2812@gmail.com>
2021-01-26 16:00:53 +0000

    checkAvailableLetters working

---
+++ Hangman/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //   Console.WriteLine("Hello Craig");
+           HangmanGame.checkAvailableLeters('b');
+        }
+
+
+    }
+
+
+
+}
